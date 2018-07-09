071cfea96e9b52cf7700139c18474206e6dc9ce4
SnaKEZiTO <renan_fengler_93@hotmail.com>
2018-07-09 15:28:21 -0300

    Progress on retrieving information from database

--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -9,6 +9,14 @@ namespace DatabaseFirst {
     class Program {
         static void Main(string[] args) {
             
+            PlutoDbContext dbContext = new PlutoDbContext();
+
+            
+            var courses = dbContext.GetCourses();
+
+            foreach (var course in courses) {
+                Console.WriteLine("Course ID: {1}{0}Course Name: {2}{0}", Environment.NewLine,  course.CourseID, course.Title);
+            }
         }
     }
     
